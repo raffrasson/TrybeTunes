4975c601a58d90b7f077258b6fb18c625afe24fd
raffrasson <raf.frasson@gmail.com>
2021-09-19 20:50:34 -0300

    completa requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -33,9 +34,15 @@ class Header extends React.Component {
       return <Loading />;
     }
     return (
-      <header data-testid="header-component">
-        <h1 data-testid="header-user-name">{ user }</h1>
-      </header>
+      <div>
+        <header data-testid="header-component">
+          <Link to="/search" data-testid="link-to-search"> Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites"> Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
+          <h1 data-testid="header-user-name">{ user }</h1>
+        </header>
+
+      </div>
     );
   }
 }
